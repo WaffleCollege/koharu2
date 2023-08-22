0dc58cbc6d6d570b8a4ef3ec8c06af757163cb30
kohapizza <koharu0929alexandros@gmail.com>
2023-08-22 20:33:53 +0900

    add files

--- P03/Lecture課題/script.js
+++ P03/Lecture課題/script.js
@@ -1,23 +1,55 @@
 'use strict'
 // Please don't delete the 'use strict' line above
 
+// ファイル全体で使用する TDD 用の変数を宣言する
+let expected;
+let actual;
+
+// 上の問題のように、関数testにチェックする内容を全てまとめる
+function test(actualExpression, expectedExpression){
+    if (actualExpression === expectedExpression) {
+        console.log("Yay! Test PASSED.");
+    } else {
+        console.error("Test FAILED. Keep trying!");
+        console.log("    actual: ", actualExpression);
+        console.log("  expected: ", expectedExpression);
+    }
+}
+//actual = /* テストを行う EXPRESSION（式）を定義する */
+//expected = /* EXPRESSION（式）の期待値を定義する */
+
+// testを呼び出す
+//test(actual, expected);
+
+
 //問題1,2
 function isEven(num){
     if(typeof num != 'number'){
         return "This is not a number.";
     }
-    else if(num % 2 === 0){
-        return true;
-    }
     else {
-        return false;
+        return num % 2 === 0;
     }
 
 }
 
-console.log(isEven(4)); // => true
-console.log(isEven(7)); // => false
-console.log(isEven("aaa")); // => "This is not a number."
+actual = isEven(5);
+expected = false;
+test(actual,expected);
+
+actual = isEven(6);
+expected = true;
+test(actual,expected);
+
+actual = isEven("aaa");
+expected = "This is not a number.";
+test(actual,expected);
+
+
+
+//console.log(isEven(4)); // => true
+//console.log(isEven(7)); // => false
+//console.log(isEven("aaa")); // => "This is not a number."
 
 //問題3
 /**
@@ -36,10 +68,19 @@ function push(array,num){
 let array = [1, 2, 3, 4];
 
 // 関数が正しい結果を返すことを確認する
-console.log(push(array, 5)); // => 5
-console.log(push(array, 10)); // => 6
+//console.log(push(array, 5)); // => 5
+//console.log(push(array, 10)); // => 6
+
+
+
+actual = push(array,5);
+expected = 5;
+test(actual,expected);
+
+actual = push(array,10);
+expected = 6;
+test(actual,expected);
 
 // 配列が正しく変更されていることを確認する
 console.log(array);
 // => [1, 2, 3, 4, 5, 10];　　
-
