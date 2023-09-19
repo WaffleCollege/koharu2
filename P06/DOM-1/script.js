16b815d254287b0d36df22a708d746f11970e9c0
kohapizza <koharu0929alexandros@gmail.com>
2023-09-19 12:21:58 +0900

    add files

--- P06/DOM-1/script.js
+++ P06/DOM-1/script.js
@@ -10,15 +10,24 @@ favoriteFood.textContent = "カレーライス";
 
 // 問題1
 // 上のを真似して、「好きなこと」も表示させましょう！
+const favoriteThings = document.getElementById("favoriteThings")
+
+favoriteThings.textContent = "ドラムを叩くこと";
 
 
 
 // 問題2
 // 日付を表示させましょう！
+const dataInfo = document.getElementById("dateInfo");
+dataInfo.textContent = "2023/9/19";
 
 
 
 
 // 問題3
 // DOMイベントを活用して、「クリックしたら背景色が変わる」ようにしよう
-
+function ColorChange(){
+    document.body.style.backgroundColor = 'green';
+}
+const changeColor = document.querySelector("#changeColor");
+changeColor.addEventListener('click',ColorChange);
