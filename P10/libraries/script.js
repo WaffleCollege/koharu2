1b8cc35246f030273ecb7fdd773438d07b008794
kohapizza <koharu0929alexandros@gmail.com>
2023-10-24 00:06:15 +0900

    add homework

---
+++ P10/libraries/script.js
@@ -0,0 +1,10 @@
+'use strict'
+// Please don't delete the 'use strict' line above
+
+$(document).ready(function(){
+    // ここにスクリプトを追加
+    $(".d-grid").on("click", function(){
+        alert("ボタンがクリックされました。");
+    });
+});
+
